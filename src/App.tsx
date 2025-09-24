520633086c126697965d7356a4a943bb3c785642
Nguyen Khoi <khoibene@gmail.com>
2025-09-24 20:17:13 +0700

    fc

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button.tsx'
+
+function App() {
+  return (
+    <div>
+      <Button>Click</Button>
+    </div>
+  )
+}
+
+export default App
